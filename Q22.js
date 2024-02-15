3e14175dad31132abd7eb7071c6fc2d5efd90f31
sadafrao <159582784+sadafrao@users.noreply.github.com>
2024-02-14 21:51:34 -0800

    Add files via upload

---
+++ Q22.js
@@ -0,0 +1,4 @@
+"use strict";
+const days = ["sunday", "monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+console.log(days[7]);
+console.log(days[5]);
---
+++ Q22.ts
@@ -0,0 +1,7 @@
+//22. Intentional Error: If you haven’t received an array index error in one of your programs yet, try to make one happen. Change an index in one of your programs to produce an index error. Make sure you correct the error before closing
+
+
+const  days : string[] = ["sunday","monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+
+console.log(days[7])
+console.log(days[5])
